524985bed358791ee7014125681895db44454c30
Federico Amura <federico.amura@viapool.com>
2018-02-05 20:35:17 -0300

    first test example

---
+++ test/first.test.js
@@ -0,0 +1,12 @@
+const chakram = require('chakram'),
+      expect = chakram.expect;
+
+
+describe("oauth-excercise", function () {
+
+  describe('first test', function () {
+    it("first test should expect true", function () {
+      expect(true).to.be.true;
+    });
+  });
+});
